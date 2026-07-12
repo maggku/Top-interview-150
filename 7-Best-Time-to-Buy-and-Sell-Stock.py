9b80adfddb523f8cece62f9b99da9048fe0de962
maggku <maggie.kuczynska@gmail.com>
2026-07-12 11:04:52 +0100

    Add the solution to the task 7

--- 7-Best-Time-to-Buy-and-Sell-Stock.py
+++ 7-Best-Time-to-Buy-and-Sell-Stock.py
@@ -28,3 +28,25 @@ Constraints:
     0 <= prices[i] <= 104
 
 """
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        min_price = prices[0]
+        max_profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            else:
+                profit = prices[i] - min_price
+                if profit > max_profit:
+                    max_profit = profit
+
+        return max_profit
+
+
+assert Solution().maxProfit([7,1,5,3,6,4]) == 5
+assert Solution().maxProfit([7,6,4,3,1]) == 0
+assert Solution().maxProfit([2,2]) == 0
