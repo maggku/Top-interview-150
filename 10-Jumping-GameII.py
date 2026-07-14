be2d8760b9a56f0ba584c4fd2555a33fcd619a4c
maggku <maggie.kuczynska@gmail.com>
2026-07-14 05:48:19 +0100

    Add solution to problem 10

--- 10-Jumping-GameII.py
+++ 10-Jumping-GameII.py
@@ -30,3 +30,22 @@ Constraints:
     It's guaranteed that you can reach nums[n - 1].
 
 """
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return 0
+
+        farthest = 0
+        jumps = 0
+        current_end = 0
+        n = len(nums)
+
+        for i in range(n - 1):
+            farthest = max(farthest, i + nums[i])
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+                if current_end >= n - 1:
+                    break
+        return jumps
