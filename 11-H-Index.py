db0b0b8ff414e387a0081da669bb21f653844813
maggku <maggie.kuczynska@gmail.com>
2026-07-15 05:34:45 +0100

    Install pytest

--- 11-H-Index.py
+++ 11-H-Index.py
@@ -26,3 +26,19 @@ Constraints:
     0 <= citations[i] <= 1000
 
 """
+
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+
+        h_index = 0
+
+        citations.sort(reverse=True)
+
+        for i in range(len(citations)):
+            if citations[i] >= i + 1:
+                h_index += 1
+            else:
+                break
+
+        return h_index
