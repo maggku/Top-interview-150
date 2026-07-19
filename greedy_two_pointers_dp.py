cf7d48a2f5dc725a207895e0c28d0ba6cb1bdb9e
maggku <maggie.kuczynska@gmail.com>
2026-07-19 07:36:12 +0100

    Add a problem where we compare greedy, dp and two pointers

---
+++ greedy_two_pointers_dp.py
@@ -0,0 +1,11 @@
+"""
+The probelm:
+given an array of integers, does there exist a pair of numbers that add up exackly to a targetT?
+Example:
+    [2, 7, 4, 1, 9, 5] target T =9 (answer yes, 2+7=9)
+"""
+#Two pointers
+
+#Greedy
+
+#DP
