c077cd51808a922f8900f3e545de2bae9b8f4fe1
maggku <maggie.kuczynska@gmail.com>
2026-07-19 15:57:02 +0100

    Add solution to 1768

--- test_1768_merge_strings_alternately.py
+++ test_1768_merge_strings_alternately.py
@@ -33,3 +33,21 @@ word2:    p   q
 merged: a p b q c   d
 
 """
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+
+        i = 0
+        j = 0
+
+        merged = []
+
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
+                merged.append(word1[i])
+                i += 1
+            if j < len(word2):
+                merged.append(word2[j])
+                j += 1
+
+        return "".join(merged)
