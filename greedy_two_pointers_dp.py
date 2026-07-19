8e70fbaa1421dae4f5aa2c3697f112ef5aaff5e6
maggku <maggie.kuczynska@gmail.com>
2026-07-19 07:53:44 +0100

    Add brute force solution to problem 151

--- greedy_two_pointers_dp.py
+++ greedy_two_pointers_dp.py
@@ -4,6 +4,22 @@ given an array of integers, does there exist a pair of numbers that add up exack
 Example:
     [2, 7, 4, 1, 9, 5] target T =9 (answer yes, 2+7=9)
 """
+
+# My solution
+
+nums = [2, 7, 4, 1, 9, 5]
+t = 9
+
+def solution(nums, t):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == t:
+                return True
+    return False
+
+
+
+
 #Two pointers
 
 #Greedy
