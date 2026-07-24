9c8aec708dbc300ade7b485f235121b969f8e580
maggku <maggie.kuczynska@gmail.com>
2026-07-24 05:35:54 +0100

    Add answer to 283

--- p_283_move_zeroes.py
+++ p_283_move_zeroes.py
@@ -25,3 +25,19 @@ Constraints:
 
 Follow up: Could you minimize the total number of operations done?
 """
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        insert_pos = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[insert_pos] = nums[i]
+                insert_pos += 1
+
+        for i in range(insert_pos, len(nums)):
+            nums[i] = 0
