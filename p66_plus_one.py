c03cbd1e2930747af766a7cae7f5375cac064807
maggku <maggie.kuczynska@gmail.com>
2026-07-25 15:33:34 +0100

    Add answer to 66

--- p66_plus_one.py
+++ p66_plus_one.py
@@ -29,3 +29,24 @@ Explanation: The array represents the integer 9.
 Incrementing by one gives 9 + 1 = 10.
 Thus, the result should be [1,0].
 """
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        n = len(digits)
+
+        for i in range(n - 1, -1, -1):
+            digits[i] += 1
+
+            if digits[i] == 10:
+                digits[i] = 0
+            else:
+                return digits
+
+        digits.insert(0, 1)
+        return digits
+
+        # all values are 9
+        # last value is 9
+        # last value is not 9
