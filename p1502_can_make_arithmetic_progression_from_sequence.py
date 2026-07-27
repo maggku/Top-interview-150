7bf2fef5ebb5763a2a64f941b2dc613ea28a555f
maggku <maggie.kuczynska@gmail.com>
2026-07-27 06:57:00 +0100

    Add solution to 1502

--- p1502_can_make_arithmetic_progression_from_sequence.py
+++ p1502_can_make_arithmetic_progression_from_sequence.py
@@ -25,3 +25,13 @@ Constraints:
     -106 <= arr[i] <= 106
 
 """
+
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        ap = arr[1] - arr[0]
+        for i in range(2, len(arr)):
+            if arr[i] - arr[i-1] != ap:
+                return False
+
+        return True
