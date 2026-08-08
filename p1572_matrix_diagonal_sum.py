a0f981df6cada147e3a4db51480648367318790a
maggku <maggie.kuczynska@gmail.com>
2026-08-08 10:14:12 +0100

    Add solution to 1572

--- p1572_matrix_diagonal_sum.py
+++ p1572_matrix_diagonal_sum.py
@@ -37,3 +37,13 @@ Constraints:
     1 <= mat[i][j] <= 100
 
 """
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        total = 0
+        for i in range(n):
+            total += mat[i][i]
+            if i != n-1-i:
+                total += mat[i][n-1-i]
+        return total
