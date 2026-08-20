edfe3aadb354f45358ceb6ec6e48d0f08f28cdc3
maggku <maggie.kuczynska@gmail.com>
2026-08-20 21:44:04 +0100

    Add math clue

--- p1232_check_if_it_isa_straight_line.py
+++ p1232_check_if_it_isa_straight_line.py
@@ -6,3 +6,5 @@ Hint
 
 You are given an integer array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
 """
+
+(y1-y0)(xi-x0)=(y1-y0)(x1-x0)
