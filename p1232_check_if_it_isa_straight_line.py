6a7865dd7a1ad6567b56e4314db5ee9448d09b18
maggku <maggie.kuczynska@gmail.com>
2026-08-20 21:40:20 +0100

    Add 1232

---
+++ p1232_check_if_it_isa_straight_line.py
@@ -0,0 +1,8 @@
+"""
+Easy
+Topics
+premium lock iconCompanies
+Hint
+
+You are given an integer array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+"""
