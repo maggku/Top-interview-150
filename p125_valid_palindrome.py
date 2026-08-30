8941e3f5359b770d127784a8a7501c591d33ad25
maggku <maggie.kuczynska@gmail.com>
2026-08-30 13:29:53 +0100

    Add solution to 125

--- p125_valid_palindrome.py
+++ p125_valid_palindrome.py
@@ -25,3 +25,23 @@ Explanation: s is an empty string "" after removing non-alphanumeric characters.
 Since an empty string reads the same forward and backward, it is a palindrome.
 
 """
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        x = 0
+        y = len(s) - 1
+
+        while x < y:
+            while x < y and not s[x].isalnum():
+                x += 1
+            while x < y and not s[y].isalnum():
+                y -= 1
+            if x < y:
+                if s[x].lower() != s[y].lower():
+                    return False
+
+                x += 1
+                y -= 1
+
+        return True
