4d22f7eee1454ba792344b7a3be738bf3bfb47f2
maggku <maggie.kuczynska@gmail.com>
2026-08-31 21:13:49 +0100

    Add solution to 26

--- p26_remove_duplicates_from_sorted_array.py
+++ p26_remove_duplicates_from_sorted_array.py
@@ -47,3 +47,19 @@ Constraints:
 
 
 """
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        if not nums:
+            k = 0
+
+        x = 1
+        k = 1
+
+        for x in range(1, len(nums)):
+            if nums[x] != nums[x-1]:
+                nums[k] = nums[x]
+                k+=1
+
+        return k
