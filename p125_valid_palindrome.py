7df88f23ab6501bb07f1faefe1dfdaef6ba301fb
maggku <maggie.kuczynska@gmail.com>
2026-09-05 10:26:11 +0100

    Add solution to 125

--- p125_valid_palindrome.py
+++ p125_valid_palindrome.py
@@ -29,19 +29,17 @@ Since an empty string reads the same forward and backward, it is a palindrome.
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        x = 0
-        y = len(s) - 1
 
-        while x < y:
-            while x < y and not s[x].isalnum():
-                x += 1
-            while x < y and not s[y].isalnum():
-                y -= 1
-            if x < y:
-                if s[x].lower() != s[y].lower():
-                    return False
+        left = 0
+        right = len(s) - 1
 
-                x += 1
-                y -= 1
-
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
         return True
