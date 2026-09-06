7e3af72acca95f67f41154c50babf3625a5da64a
maggku <maggie.kuczynska@gmail.com>
2026-09-06 13:50:39 +0100

    Solution to 11

--- p11_container_with_most_water.py
+++ p11_container_with_most_water.py
@@ -34,3 +34,18 @@ Constraints:
     2 <= n <= 105
     0 <= height[i] <= 104
 """
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        best = 0
+
+        while left < right:
+            lower_height = min([height[left], height[right]])
+            best = max([best, lower_height*(right-left)])
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return best
