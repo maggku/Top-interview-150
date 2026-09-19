a76b79f5e6c1f79bbd0a97405f6916c494532908
maggku <maggie.kuczynska@gmail.com>
2026-09-19 11:23:34 +0100

    Problem and solution in two pointers summary, p11

--- ptwo_pointers_summary.py
+++ ptwo_pointers_summary.py
@@ -17,7 +17,36 @@ def twosum(array):
 
     return []
 
-"""Valid Palindrome"""
+"""Valid Palindrome
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+ 
+
+"""
 
 def palindrome(array):
     left = 0
@@ -29,4 +58,46 @@ def palindrome(array):
         else:
             return False
     return True
+
+
+"""
+Container with most water. problem 11
+
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+"""
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height)-1
+        best = 0
+        while left < right:
+            h = min(height[left], height[right])
+            best = max(best, h * (right - left))
+
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+        return best
+
 
