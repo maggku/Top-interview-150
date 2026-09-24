522730de9e9b0e786d02f159dac03c1256a9b728
maggku <maggie.kuczynska@gmail.com>
2026-09-24 05:38:10 +0100

    Add solution to 205

--- p205_isomorphic_strings.py
+++ p205_isomorphic_strings.py
@@ -37,3 +37,20 @@ Input: s = "paper", t = "title"
 Output: true
 
 """
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+        s_to_t, t_to_s = {}, {}
+
+        for a, b in zip(s, t):
+            if a in s_to_t and s_to_t[a] != b:
+                return False
+            elif b in t_to_s and t_to_s[b] != a:
+                return False
+            else:
+                s_to_t[a] = b
+                t_to_s[b] = a
+
+        return True
