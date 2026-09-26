7601cd26d4a23ae0a2b6cfb2c1daef0a2373ee3d
maggku <maggie.kuczynska@gmail.com>
2026-09-26 15:33:50 +0100

    Add solution to 49

--- p49_group_anagrams.py
+++ p49_group_anagrams.py
@@ -37,3 +37,14 @@ Constraints:
 
 
 """
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+
+
+        groups = defaultdict(list)
+        for s in strs:
+            key = ''.join(sorted(s))
+            groups[key].append(s)
+        return list(groups.values())
